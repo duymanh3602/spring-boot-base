a5e5ae12e89e780a508b75eab7c9d0e488cb161b
duymanh3602 <20020438@vnu.edu.vn>
2024-04-26 15:13:19 +0700

    refactor: init spring base

---
+++ server-base/src/main/java/com/manhdd/serverbase/ServerBaseApplication.java
@@ -0,0 +1,13 @@
+package com.manhdd.serverbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBaseApplication.class, args);
+    }
+
+}
---
+++ server-base/src/test/java/com/manhdd/serverbase/ServerBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhdd.serverbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
